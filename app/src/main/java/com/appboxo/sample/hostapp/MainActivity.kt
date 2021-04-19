2220d6337f641ed9b95ce9dbe6a3a0a78810f80b
Kanimet Aitkulov <kani.aitkulov@gmail.com>
2021-04-19 15:20:41 +0600

    updates sdk

--- app/src/main/java/com/appboxo/sample/hostapp/MainActivity.kt
+++ app/src/main/java/com/appboxo/sample/hostapp/MainActivity.kt
@@ -1,14 +1,12 @@
 package com.appboxo.sample.hostapp
 
-import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.appboxo.sdk.Appboxo
+import com.appboxo.sdk.Config
 import com.appboxo.sdk.Miniapp
-import com.google.android.gms.tasks.OnCompleteListener
-import com.google.firebase.iid.FirebaseInstanceId
+import com.appboxo.sdk.MiniappConfig
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -18,7 +16,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         demo.setOnClickListener {
-            Appboxo.getMiniapp("app16973", "YOUR_AUTH_PAYLOAD")
+            Appboxo.getMiniapp("app16973")
                 .setCustomEventListener { activity, miniapp, customEvent ->
                     AlertDialog.Builder(activity)
                         .setMessage(customEvent.payload.toString())
@@ -36,7 +34,7 @@ class MainActivity : AppCompatActivity() {
         }
 
         skyscanner.setOnClickListener {
-            Appboxo.getMiniapp("app85076", "YOUR_AUTH_PAYLOAD")
+            Appboxo.getMiniapp("app85076")
                 .setLifecycleListener(object : Miniapp.LifecycleListener {
                     override fun onLaunch(miniapp: Miniapp) {
                         //Called when the miniapp will launch with Appboxo.open(...)
@@ -60,49 +58,6 @@ class MainActivity : AppCompatActivity() {
                 .open(this)
         }
 
-        appboxo_store.setOnClickListener {
-            Appboxo.getMiniapp("app36902", "YOUR_AUTH_PAYLOAD")
-                .setCustomEventListener { activity, miniapp, customEvent ->
-                    val price = customEvent.payload["price"] as Int
-                    val name = customEvent.payload["name"] as String
-                    val currency = customEvent.payload["currency"] as String
-                    StripeDialog(activity, price, currency, name).apply {
-                        doOnSuccess = {
-                            customEvent.payload = mapOf("payment" to "received")
-                            miniapp.sendEvent(customEvent)
-                        }
-                        activity.doOnActivityResult { requestCode, resultCode, data ->
-                            this.onActivityResult(requestCode, resultCode, data)
-                        }
-                    }.show()
-                }
-                .open(this)
-        }
-
-        checkPush(intent)
-
-        FirebaseInstanceId.getInstance().instanceId
-            .addOnCompleteListener(OnCompleteListener { task ->
-                if (!task.isSuccessful) return@OnCompleteListener
-                val token = task.result?.token
-                Log.d("TOKEN", token)
-            })
-    }
-
-    override fun onResume() {
-        super.onResume()
-    }
-
-    override fun onNewIntent(intent: Intent) {
-        super.onNewIntent(intent)
-        checkPush(intent)
-    }
-
-    private fun checkPush(intent: Intent) {
-        val appId = intent.getStringExtra("miniapp_id") ?: ""
-        if (appId.isNotBlank()) {
-            Appboxo.getMiniapp(appId, "YOUR_AUTH_PAYLOAD").open(this)
-        }
     }
 
 }
